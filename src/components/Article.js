3f46e71e836b17309f4124ab727b5595e90a30a6
timkozemzak@gmail.com <timkozemzak@gmail.com>
2021-01-18 13:10:15 -0700

    init

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Article(props) {
+    return (
+        <article>
+            <a href={ "https://www.reddit.com" + props.article.permalink} target="blank">
+                <h3>{props.article.title}</h3>
+            </a>
+        </article>
+    )
+}
+
+export default Article;
+
